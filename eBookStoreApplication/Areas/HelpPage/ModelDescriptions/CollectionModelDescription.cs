59208999b6d03ade27a0395da4a70656fe2b53a3
veerabhadraswamy.nk <veerabhadraswamynk@gmail.com>
2020-02-04 00:11:11 +0530

    clone 2

---
+++ eBookStoreApplication/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace eBookStoreApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ eBookStoreApplication/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace eBookStoreApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
